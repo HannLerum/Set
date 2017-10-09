970fcd03c48ecbfe07aaca0b0e0327857d738398
HannLerum <32527175+HannLerum@users.noreply.github.com>
2017-10-09 15:12:50 -0600

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,4 +1,38 @@
 
 public class Card {
-
+  
+  int cardsInASet;
+  int numberOfVariables;
+  int[] variables;
+  boolean selected;
+  
+  public Card()
+  {
+    cardsInASet = 3;
+    numberOfVariables = 4;
+    variables = new int[numberOfVariables];
+    for(int i =0; i<numberOfVariables; i++)
+    {
+      variables[i]=0;
+    }
+    selected = false;
+  }
+  
+  public Card(int numberOfVariables,int numberOfCardstoaSet,int[] variables)
+  {
+    cardsInASet = numberOfCardstoaSet;
+    numberOfVariables = numberOfVariables;
+    this.variables = new int[numberOfVariables];
+    for(int i =0; i<numberOfVariables; i++)
+    {
+      this.variables[i]=variables[i];
+    }
+    selected = false;
+  }
+  
+  public boolean isSelected()
+  {
+    return selected
+  }
+  
 }
