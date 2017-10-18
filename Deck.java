6be8d7d85487309f47efb06831beedef4cbf7001
Hann Lerum <hlerum@carroll.edu>
2017-10-18 15:11:15 -0600

    Deck completed (hopefully)

--- Deck.java
+++ Deck.java
@@ -11,27 +11,61 @@ public class Deck {
 	
 	Deck()
 	{
-		NUMBER_OF_CARDS = 3;
-		deck = new Card[NUMBER_OF_CARDS];
-		
+		this(4,3);
 	}
 	
 	Deck(int numOfVariables, int cardsInASet)
 	{
+		initialize(numOfVariables, cardsInASet);
+	}
+	
+	private void initialize(int numOfVariables, int cardsInASet)
+	{
 		NUMBER_OF_CARDS = (int)Math.pow(cardsInASet,numOfVariables);
 		deck = new Card[NUMBER_OF_CARDS];
 		
+		for(int i = 0 ; i<NUMBER_OF_CARDS ; i++)
+		{
+			int[] variables = new int[numOfVariables];
+			for(int j = 0; j<numOfVariables; j++)
+			{
+				variables[j]= (i/(int)Math.pow(cardsInASet,j))%cardsInASet;
+				//if the variable is the number of tokens on the card
+				if(j==0)
+				{
+					variables[j]++;
+				}
+			}
+			deck[i] = new Card(numOfVariables,cardsInASet,variables);
+		}
 		
+		currentCard = 0;
 	}
 	
 	public void shuffle()
 	{
+		currentCard = 0;
 		
+		for(int first = 0; first < deck.length; first++)
+		{
+			int second = randomNumbers.nextInt(NUMBER_OF_CARDS);
+			
+			Card temp = deck[first];
+			deck[first] = deck[second];
+			deck[second] = temp;
+		}
 	}
 	
-	public void deal()
+	public Card deal()
 	{
-		
+		if(currentCard < deck.length)
+		{
+			return deck[currentCard++];
+		}
+		else
+		{
+			return null;
+		}
 	}
 	
 	public int numberOfCards()
